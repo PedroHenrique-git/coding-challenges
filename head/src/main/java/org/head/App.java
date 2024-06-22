42c6a0d00f07e24b716a2ddfbb8dfbed72e97579
PedroHenrique-git <pedromf34@outlook.com>
2024-06-22 19:58:59 -0300

    feat: finish head

--- head/src/main/java/org/head/App.java
+++ head/src/main/java/org/head/App.java
@@ -3,6 +3,7 @@ package org.head;
 
 import org.lib.Head;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class App
 {
@@ -14,13 +15,17 @@ public class App
                 List<String> listArgs = List.of(args);
 
                 String linesToShowParameter = listArgs.stream().filter(arg -> arg.matches("-n\\d*")).findFirst().orElse("");
+                String bytesToShowParameter = listArgs.stream().filter(arg -> arg.equals("-c")).findFirst().orElse("");
+
                 int linesToShow = linesToShowParameter.isEmpty() ? 10 : Integer.parseInt(linesToShowParameter.split("-n")[1]);
+                int bytesToShow = bytesToShowParameter.isEmpty() ? -1 : Integer.parseInt(listArgs.get(listArgs.indexOf("-c") + 1));
 
-                String file = listArgs.get(listArgs.size() - 1);
+                List<String> files = listArgs.stream().filter(arg -> arg.matches("(.*)\\.(.*)")).collect(Collectors.toList());
 
-                head = new Head(file, linesToShow);
+                head = bytesToShow > 0 ? new Head(files, 0, bytesToShow) : new Head(files, linesToShow);
+                String output = bytesToShow > 0 ? head.processInputByNumberOfBytes() : head.processInput();
 
-                System.out.println(head.processInput());
+                System.out.println(output);
 
                 return;
             }
--- head/src/main/java/org/lib/Head.java
+++ head/src/main/java/org/lib/Head.java
@@ -1,29 +1,79 @@
 package org.lib;
 
 import java.io.*;
+import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
+import java.util.stream.IntStream;
 
 public class Head {
-    private String file;
+    private List<String> files;
     private int linesToShow = 10;
+    private int bytesToShow = 31;
 
     public Head() {}
 
-    public Head(String file, int linesToShow) {
-        this.file = file;
+    public Head(List<String> files) {
+        this.files = files;
+    }
+
+    public Head(List<String> files, int linesToShow) {
+        this.files = files;
         this.linesToShow = linesToShow;
     }
 
+    public Head(List<String> files, int linesToShow, int bytesToShow) {
+        this.files = files;
+        this.linesToShow = linesToShow;
+        this.bytesToShow = bytesToShow;
+    }
+
     public String processInput() throws IOException {
-        List<String> fileData = readFile();
+        List<List<String>> filesData = readFile();
 
         StringBuilder b = new StringBuilder();
 
-        fileData.subList(0, Math.min(linesToShow, fileData.size())).forEach(line -> b.append(String.format("%s\n", line)));
+        filesData.forEach(fileData -> {
+            StringBuilder fileInfoResult = new StringBuilder();
+
+            fileInfoResult.append(String.format("%s\n", fileData.get(0)));
+            fileData.subList(1, Math.min(linesToShow, fileData.size())).forEach(line -> fileInfoResult.append(String.format("%s\n", line)));
+
+            fileInfoResult.append("\n\n");
+
+            b.append(fileInfoResult);
+        });
+
+        return b.toString();
+    }
+
+    public String processInputByNumberOfBytes() throws IOException {
+        List<List<String>> filesData = readFile();
+
+        StringBuilder b = new StringBuilder();
+
+        filesData.forEach(fileData -> {
+            StringBuilder textBuilder = new StringBuilder();
+
+            String fileName = fileData.get(0);
+
+            IntStream.range(0, fileData.size()).forEach(index -> {
+                String line = fileData.get(index);
+
+                if(index > 0)
+                    textBuilder.append(String.format("%s\n", line));
+            });
+
+            textBuilder.append("\n\n");
+
+            String textByBytes = new String(
+                    Arrays.copyOfRange(textBuilder.toString().getBytes(StandardCharsets.UTF_8), 0, bytesToShow),
+                    StandardCharsets.UTF_8
+            );
+
+            b.append(fileName.trim()).append("\n").append(textByBytes);
+        });
 
         return b.toString();
     }
@@ -37,7 +87,23 @@ public class Head {
         }
     }
 
-    public List<String> readFile() throws IOException {
-        return  Files.readAllLines(new File(file).toPath(), StandardCharsets.UTF_8);
+    public List<List<String>> readFile() {
+        List<List<String>> filesData = new ArrayList<>();
+
+        files.forEach(file -> {
+            try {
+                File f = new File(file);
+                List<String> fileData = new ArrayList<>();
+
+                fileData.add("==> " + f.getName()  + " <==");
+                fileData.addAll(Files.readAllLines(f.toPath(), StandardCharsets.UTF_8));
+
+                filesData.add(fileData);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        });
+
+        return filesData;
     }
 }
