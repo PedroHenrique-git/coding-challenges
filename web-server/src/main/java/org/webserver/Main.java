372812239a82f0601b50dbb1eabc5fed79468a56
PedroHenrique-git <pedromf34@outlook.com>
2024-07-16 19:24:00 -0300

    feat: http server

--- web-server/src/main/java/org/webserver/Main.java
+++ web-server/src/main/java/org/webserver/Main.java
@@ -1,13 +1,5 @@
 package org.webserver;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.InetSocketAddress;
-import java.net.ServerSocket;
-import java.net.Socket;
-
 public class Main {
     public static void main(String[] args) {
         new SimpleHttpServer().start();
--- web-server/src/main/java/org/webserver/SimpleHttpServer.java
+++ web-server/src/main/java/org/webserver/SimpleHttpServer.java
@@ -1,12 +1,12 @@
 package org.webserver;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Logger;
 
@@ -14,17 +14,49 @@ public class SimpleHttpServer {
     private final Logger logger = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
 
     public void start() {
+        int SERVER_PORT = 5000;
+
         try(ServerSocket server = new ServerSocket()) {
-            server.bind(new InetSocketAddress(5000));
+            server.bind(new InetSocketAddress(SERVER_PORT));
 
             while(true) {
                 Socket client = server.accept();
 
-                InputStream stream = client.getInputStream();
+                BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+                BufferedWriter out = new BufferedWriter(new OutputStreamWriter(client.getOutputStream()));
 
-                List<String> requestHeaders = readIncomingStream(stream);
+                List<String> headers = readInput(in);
+                String[] httpConfig = readeBaseHttpConfig(headers);
 
-                System.out.println(requestHeaders);
+                String path = httpConfig[1];
+
+                List<String> indexMap = List.of("/", "/index", "/index.html");
+
+                if(indexMap.contains(path)) {
+                    out.write("HTTP/1.1 200 OK\r\n");
+                    out.write("Content-type: text/html\r\n");
+                    out.write("\r\n");
+
+                    out.write(readFileContent("index.html"));
+                } else {
+                    String content = readFileContent(path);
+
+                    if(content.isEmpty()) {
+                        out.write("HTTP/1.1 404\r\n");
+                        out.write("Content-type: text/plain\r\n");
+                        out.write("\r\n");
+                        out.write("Page not found");
+                    } else {
+                        out.write("HTTP/1.1 200 OK\r\n");
+                        out.write("Content-type: text/html\r\n");
+                        out.write("\r\n");
+                        out.write(content);
+                    }
+                }
+
+                out.close();
+                in.close();
+                client.close();
             }
         } catch (IOException e) {
             logger.info("Something went wrong during the server initialization");
@@ -32,11 +64,35 @@ public class SimpleHttpServer {
         }
     }
 
-    private List<String> readIncomingStream(InputStream stream) {
-        BufferedReader reader = new BufferedReader(
-                new InputStreamReader(stream)
-        );
+    private List<String> readInput(BufferedReader reader) throws IOException {
+        List<String> headers = new ArrayList<>();
 
-        return reader.lines().toList();
+        String header;
+
+        while ((header = reader.readLine()) != null) {
+            headers.add(header);
+
+            if(header.isEmpty()) {
+                break;
+            }
+        }
+
+        return headers;
+    }
+
+    private String[] readeBaseHttpConfig(List<String> headers) {
+        return headers.get(0).split(" ");
+    }
+
+    private String readFileContent(String fileName) {
+        String STATIC_FOLDER = "www";
+
+        try {
+            return Files.readString(Path.of(String.format(STATIC_FOLDER + "/" + fileName)));
+        } catch (IOException e) {
+            logger.info("something went wrong when trying to read the file " + e.getMessage());
+
+            return "";
+        }
     }
 }
